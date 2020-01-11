c8ce6e13c467cf70f5025361640d399dc58a57f1
dsabrinarg <sabrina@vauxoo.com>
2020-01-10 20:10:15 -0600

    [ADD] Customer app module added.

---
+++ ehcabello/README.md
@@ -0,0 +1,13 @@
+Module to initialize a db:
+-------------------------
+
+This module contains the init data to Eléctrica Hidráulica Cabello Company
+
+Repositories needed for this module:
+====================================
+
+1. enterprise git@github.com:Vauxoo/enterprise.git
+2. mexico git@git.vauxoo.com:Vauxoo/mexico.git
+
+
+Just declare all this repositories on 12.0 branches in your addons-path and install "ehcabello" module.
---
+++ ehcabello/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Vauxoo
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Eléctrica Hidráulica Cabello company',
+    'summary': '''
+    Instance creator for Eléctrica Hidráulica Cabello. This is the app.
+    ''',
+    'author': 'Vauxoo',
+    'website': 'https://www.vauxoo.com',
+    'license': 'AGPL-3',
+    'category': 'Installer',
+    'version': '12.0.0.0.1',
+    'depends': [
+        'decimal_precision',
+    ],
+    'test': [
+    ],
+    'data': [
+        # data
+        'data/res_currency_data.xml',
+        # views
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
